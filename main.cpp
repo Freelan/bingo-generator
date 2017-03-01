ba3ab01813de5d2f0b84eb0102aad388c0a0c86c
Freelan <lunghshtern@gmail.com>
2017-03-01 18:28:17 +0100

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 #include <string>
 #include <fstream>
 #include <ctime>
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <unistd.h>
 
 bool loadStrings( std::string fileName, std::vector<std::string>& strings )
 {
@@ -29,104 +31,140 @@ bool loadStrings( std::string fileName, std::vector<std::string>& strings )
 
 int main( int argc, char *argv[] )
 {
-	
+    int arg;
+    opterr = 0;
+
+    char* iValue = NULL;
+    char* source = NULL;
+    int copies = 1;
+
+    while( ( arg = getopt( argc, argv, "i:s:" ) ) != -1 )
+    {
+        switch( arg )
+        {
+            case 'i':
+                iValue = optarg;
+                copies = (int)iValue[0] - 48;
+                break;
+            case 's':
+                source = optarg;
+                break;
+            case '?':
+                if( optopt == 'i' )
+                    std::cout << "Option requires an argument." << std::endl;
+                else if( isprint( optopt ) )
+                    std::cout << "Unkown option." << std::endl;
+                else
+                    std::cout << "Unkown option character" << std::endl;
+                return 1;
+                break;
+        }
+    }
+
+
     srand( time( NULL ) );
-    sf::RenderWindow window( sf::VideoMode(500, 500), "Bingo Generator" );
     sf::Image screen;
 
     std::vector<std::string> strings;
-    if( !loadStrings( "smieszne2.txt", strings ) )
+    if( !loadStrings( source, strings ) )
         return 0;
     
     strings.shrink_to_fit();
     int len = strings.size()-1;
-    std::cout << len << std::endl;
-    bool wasDrawn[len] = { false };
-    int whichLine[len];
-    int number;
+    //std::cout << len << std::endl;
     
-    for( int i = 0; i < len; i++ )
+    
+    for( int k = 0; k < copies; k++ )
     {
-        do
-        {
-            number = std::rand() % len;
-            whichLine[i] = number;
-        } while( wasDrawn[number] == true );
-        wasDrawn[number] = true;
-    }
-
-    while( window.isOpen() )
-    {
-        sf::Event event;
-        while( window.pollEvent( event ) )
-        {
-            if( event.type == sf::Event::Closed )
-                window.close();
-        }
-        window.clear( sf::Color::White );
+        bool wasDrawn[len] = { false };
+        int whichLine[len] = { 0 };
+        int number;
         
-        //draw lines
-        sf::VertexArray lines( sf::LineStrip, 2 );
-        lines[0].color = sf::Color::Black;
-        lines[1].color = sf::Color::Black;
-        
-        for( int i = 1; i < 5; i++ )
+        for( int i = 0; i < len; i++ )
         {
-            lines[0].position = sf::Vector2f(i*100, 0);
-            lines[1].position = sf::Vector2f(i*100, 500);
-
-            window.draw( lines );
-        }   
-        for( int i = 1; i < 5; i++ )
-        {
-            lines[0].position = sf::Vector2f(0, i*100);
-            lines[1].position = sf::Vector2f(500, i*100);
-
-            window.draw( lines );
-        }   
-
-        //draw text
-        sf::Font font;
-        if( !font.loadFromFile( "DejaVuSansMono.ttf" ) )
-        {
-            return 0;
+            do
+            {
+                number = std::rand() % len;
+                whichLine[i] = number;
+            } while( wasDrawn[number] == true );
+            wasDrawn[number] = true;
         }
 
-        sf::Text text;
-        text.setFont( font );
-        text.setCharacterSize( 12 );
-        text.setFillColor( sf::Color::Black );
-        
-        int y = 0;
-        int z = 0;
-        for( int i = 0; i < 29; i++, z++ )
+        sf::RenderWindow window( sf::VideoMode(500, 500), "Bingo Generator" );
+        while( window.isOpen() )
         {
-            if( i != 12 )
+            sf::Event event;
+            while( window.pollEvent( event ) )
             {
-                text.setString( strings[ whichLine[i] ].c_str() );
-            }else
+                if( event.type == sf::Event::Closed )
+                    window.close();
+            }
+            window.clear( sf::Color::White );
+            
+            //draw lines
+            sf::VertexArray lines( sf::LineStrip, 2 );
+            lines[0].color = sf::Color::Black;
+            lines[1].color = sf::Color::Black;
+            
+            for( int i = 1; i < 5; i++ )
             {
-                text.setString( "\nBONUS" );
+                lines[0].position = sf::Vector2f(i*100, 0);
+                lines[1].position = sf::Vector2f(i*100, 500);
+
+                window.draw( lines );
+            }   
+            for( int i = 1; i < 5; i++ )
+            {
+                lines[0].position = sf::Vector2f(0, i*100);
+                lines[1].position = sf::Vector2f(500, i*100);
+
+                window.draw( lines );
+            }   
+
+            //draw text
+            sf::Font font;
+            if( !font.loadFromFile( "DejaVuSansMono.ttf" ) )
+            {
+                return 0;
             }
 
-            //text.setPosition( z*100, y*100 );
+            sf::Text text;
+            text.setFont( font );
+            text.setCharacterSize( 12 );
+            text.setFillColor( sf::Color::Black );
             
-            text.setPosition( 50+z*100 - text.getGlobalBounds().width / 2, y*100 );
-
-            if( z % 4 == 0 && z != 0 )
+            int y = 0;
+            int z = 0;
+            for( int i = 0; i < 29; i++, z++ )
             {
-                z = -1;
-                y++;
+                if( i != 12 )
+                {
+                    text.setString( strings[ whichLine[i] ].c_str() );
+                }else
+                {
+                    text.setString( "\nBONUS" );
+                }
+
+                text.setPosition( 50+z*100 - text.getGlobalBounds().width / 2, y*100 );
+
+                if( z % 4 == 0 && z != 0 )
+                {
+                    z = -1;
+                    y++;
+                }
+                
+                window.draw( text );
             }
-            
-            window.draw( text );
+            window.display();
+            screen = window.capture();
         }
-        window.display();
-        screen = window.capture();
-    }
+        
+        screen.flipVertically();
+        std::string outputName = "out";
+        outputName = outputName + std::to_string(k+1) + ".png";
+        screen.saveToFile( outputName.c_str() ); 
     
-    screen.flipVertically();
-    screen.saveToFile( "3es22t.png" ); 
+    }
 
     return 0;
 }
